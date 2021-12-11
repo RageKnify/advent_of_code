08114e5907616ab90395c4de10c7cb1ddbb8eb51
RageKnify <RageKnify@gmail.com>
2021-12-11 06:39:09 +0100

    day11: Parts 1 & 2

---
+++ 2021/day11/src/part1.rs
@@ -0,0 +1,78 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn step(octupi: &mut Vec<Vec<u32>>) -> usize {
+    for x in 0..octupi.len() {
+        for y in 0..octupi[0].len() {
+            octupi[x][y] += 1;
+        }
+    }
+    let mut flashes = 0;
+    let max_x = octupi.len() - 1;
+    let max_y = octupi[0].len() - 1;
+    loop {
+        let mut flashed = false;
+        for x in 0..octupi.len() {
+            for y in 0..octupi[0].len() {
+                if octupi[x][y] >= 10 && octupi[x][y] < 23 {
+                    octupi[x][y] = 23;
+                    flashed = true;
+                    flashes += 1;
+                    if x > 0 {
+                        octupi[x - 1][y] += 1;
+                    }
+                    if x < max_x {
+                        octupi[x + 1][y] += 1;
+                    }
+                    if y > 0 {
+                        octupi[x][y - 1] += 1;
+                    }
+                    if y < max_y {
+                        octupi[x][y + 1] += 1;
+                    }
+                    if x > 0 && y > 0 {
+                        octupi[x - 1][y - 1] += 1;
+                    }
+                    if x > 0 && y < max_y {
+                        octupi[x - 1][y + 1] += 1;
+                    }
+                    if x < max_x && y > 0 {
+                        octupi[x + 1][y - 1] += 1;
+                    }
+                    if x < max_x && y < max_y {
+                        octupi[x + 1][y + 1] += 1;
+                    }
+                }
+            }
+        }
+        if !flashed {
+            break;
+        }
+    }
+    for x in 0..octupi.len() {
+        for y in 0..octupi[0].len() {
+            if octupi[x][y] > 9 {
+                octupi[x][y] = 0;
+            }
+        }
+    }
+    flashes
+}
+
+fn main() -> std::io::Result<()> {
+    let mut octupi: Vec<Vec<u32>> = BufReader::new(File::open("input.txt")?)
+        .lines()
+        .map(Result::unwrap)
+        .map(|s| {
+            s.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect();
+
+    let res: usize = (0..100u8).map(|_| step(&mut octupi)).sum();
+    println!("Result = {}", res);
+
+    Ok(())
+}
