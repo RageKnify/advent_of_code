f01df41932871b0962c1b6d0db80077ead7ab515
RageKnify <RageKnify@gmail.com>
2022-12-05 10:02:27 +0000

    2022: day05: remove comment with structure

--- 2022/day05/src/part1.rs
+++ 2022/day05/src/part1.rs
@@ -1,15 +1,5 @@
 use std::fs::read_to_string;
 
-//     [P]                 [C] [C]                                          
-//     [W]         [B]     [G] [V] [V]                                      
-//     [V]         [T] [Z] [J] [T] [S]                                      
-//     [D] [L]     [Q] [F] [Z] [W] [R]                                      
-//     [C] [N] [R] [H] [L] [Q] [F] [G]                                      
-// [F] [M] [Z] [H] [G] [W] [L] [R] [H]                                      
-// [R] [H] [M] [C] [P] [C] [V] [N] [W]                                      
-// [W] [T] [P] [J] [C] [G] [W] [P] [J]                                      
-//  1   2   3   4   5   6   7   8   9
-
 fn main() -> std::io::Result<()> {
     let file_content = read_to_string("input.txt")?;
 
