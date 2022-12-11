9c6410d806f504aa2e0525696cd4ade1e427bf4d
RageKnify <RageKnify@gmail.com>
2022-12-11 18:34:59 +0000

    2022: day01: const Part 1

--- 2022/day01/src/part1.rs
+++ 2022/day01/src/part1.rs
@@ -1,19 +1,48 @@
-use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
+#![feature(const_for)]
+#![feature(const_slice_index)]
+#![feature(const_option)]
 
-fn main() -> std::io::Result<()> {
-    let input = BufReader::new(File::open("input.txt")?);
+const fn parse_u32(line: &[u8]) -> Option<u32> {
+    if line.is_empty() {
+        return None;
+    }
+    let mut res: u32 = 0;
+    let mut idx = 0;
+    let len = line.len();
+    while idx < len {
+        let b = line[idx];
+        res *= 10;
+        res += (b - b'0') as u32;
+        idx += 1;
+    }
+    Some(res)
+}
 
+const fn find_nl(bytes: &[u8]) -> usize {
+    let mut idx = 0;
+    loop {
+        if bytes[idx] == b'\n' {
+            return idx;
+        }
+        idx += 1;
+    }
+}
+
+const fn calculate(input: &str) -> u32 {
     let mut max = 0;
     let mut curr = 0;
 
-    for line in input.lines().map(Result::unwrap) {
-        match line.parse::<u32>() {
-            Ok(n) => {
+    let input_bytes = input.as_bytes();
+
+    let mut start = 0;
+    let mut end: usize = find_nl(input_bytes);
+    while end < input_bytes.len() {
+        let line = input_bytes.get(start..end).unwrap();
+        match parse_u32(line) {
+            Some(n) => {
                 curr += n;
             }
-            Err(_) => {
+            None => {
                 // this is a blank line separating 2 elves
                 if curr > max {
                     max = curr;
@@ -21,12 +50,25 @@ fn main() -> std::io::Result<()> {
                 curr = 0;
             }
         }
+        start = end + 1;
+        let rest = input_bytes.get(end + 1..).unwrap();
+        if rest.is_empty() {
+            break;
+        }
+        let next_ln = find_nl(rest);
+        end += 1 + next_ln;
     }
     // checking the last elf
     if curr > max {
         max = curr;
     }
 
-    println!("Result = {}", max);
-    Ok(())
+    max
+}
+
+fn main() {
+    const INPUT: &str = include_str!("../input.txt");
+
+    const RES: u32 = calculate(INPUT);
+    println!("Result = {}", RES);
 }
